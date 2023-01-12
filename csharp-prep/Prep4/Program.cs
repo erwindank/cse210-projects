408982c6ef1a16ae5faf7488a3a1116f34142e58
erwindank <erwindank@gmail.com>
2023-01-12 00:45:23 -0600

    C# Prep 4 Done

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,11 +5,10 @@ class Program
     static void Main(string[] args)
     {
         List<int> numbers = new List<int>();
-        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
         int userNumber = -1;
         while (userNumber != 0)
         {
-            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
             Console.Write("Enter Number: ");
             
             string userResponse = Console.ReadLine();
