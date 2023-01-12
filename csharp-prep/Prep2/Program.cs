73559f1f93fc50030ad8c888c3fe303c3ad85506
erwindank <erwindank@gmail.com>
2023-01-12 00:06:56 -0600

    C# Prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int percentage = int.Parse(grade);
+
+        string letter_grade = "";
+
+        if (percentage >= 90)
+        {
+            letter_grade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter_grade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter_grade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter_grade = "D";
+        }
+        else
+        {
+            letter_grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter_grade}");
+        
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
