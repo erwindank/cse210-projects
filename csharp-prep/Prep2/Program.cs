e648742d98c84769ea95eaf014d7e520ce841444
erwindank <erwindank@gmail.com>
2023-02-25 16:35:01 -0600

    Made adjustments to the code.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,42 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-Console.Write("What is your grade percentage? ");
-        string grade = Console.ReadLine();
-        int percentage = int.Parse(grade);
+        // This is prep 2
+        Console.Write("What is your grade percentage? ");
+                string grade = Console.ReadLine();
+                int percentage = int.Parse(grade);
 
-        string letter_grade = "";
+                string letter_grade = "";
 
-        if (percentage >= 90)
-        {
-            letter_grade = "A";
-        }
-        else if (percentage >= 80)
-        {
-            letter_grade = "B";
-        }
-        else if (percentage >= 70)
-        {
-            letter_grade = "C";
-        }
-        else if (percentage >= 60)
-        {
-            letter_grade = "D";
-        }
-        else
-        {
-            letter_grade = "F";
-        }
+                if (percentage >= 90)
+                {
+                    letter_grade = "A";
+                }
+                else if (percentage >= 80)
+                {
+                    letter_grade = "B";
+                }
+                else if (percentage >= 70)
+                {
+                    letter_grade = "C";
+                }
+                else if (percentage >= 60)
+                {
+                    letter_grade = "D";
+                }
+                else
+                {
+                    letter_grade = "F";
+                }
 
-        Console.WriteLine($"Your grade is: {letter_grade}");
-        
-        if (percentage >= 70)
-        {
-            Console.WriteLine("You passed!");
-        }
-        else
-        {
-            Console.WriteLine("Better luck next time!");
-        }
+                Console.WriteLine($"Your grade is: {letter_grade}");
+                
+                if (percentage >= 70)
+                {
+                    Console.WriteLine("You passed!");
+                }
+                else
+                {
+                    Console.WriteLine("Better luck next time!");
+                }
     }
 }
