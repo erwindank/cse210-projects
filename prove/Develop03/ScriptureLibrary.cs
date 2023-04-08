0626c5736a0ec085619f696dd6804a4228e55a9f
erwindank <erwindank@gmail.com>
2023-04-08 00:23:28 -0600

    Comments on code for ScriptureLibrary Class

--- prove/Develop03/ScriptureLibrary.cs
+++ prove/Develop03/ScriptureLibrary.cs
@@ -4,14 +4,20 @@ using System.Collections.Generic;
 class ScriptureLibrary
 {
     private readonly List<Scripture> _scriptures = new List<Scripture>();
-    private int _currentScriptureIndex = -1;
+    
+    // Variable to keep track of the index of the current scripture, It starts off as "-1" because there's no scripture selected yet.
+    private int _currentScriptureIndex = -1;    
+    // To generate a random number used to get a random scripture.
     private readonly Random _random = new Random();
     
+    // Adds a scripture to the library.
     public void AddScripture(Scripture scripture)
     {
         _scriptures.Add(scripture);
     }
     
+    // Returns the current scripture.
+    // Throws an exception if no scripture is currently selected.
     public Scripture GetCurrentScripture()
     {
         if (_currentScriptureIndex == -1)
@@ -22,11 +28,14 @@ class ScriptureLibrary
         return _scriptures[_currentScriptureIndex];
     }
     
+    // Returns the total number of scriptures in the library.
     public int GetTotalScriptures()
     {
         return _scriptures.Count;
     }
     
+    // Returns a random scripture from the library.
+    // Throws an exception if there are no scriptures in the library.
     public Scripture GetRandomScripture()
     {
         if (_scriptures.Count == 0)
@@ -34,10 +43,12 @@ class ScriptureLibrary
             throw new InvalidOperationException("There are no scriptures in the library.");
         }
         
+        // Generate a random index within the range of the number of scriptures in the library.
         int index = _random.Next(_scriptures.Count);
         return _scriptures[index];
     }
-
+    
+    // Removes a scripture from the library 
     internal void RemoveScripture(Scripture scripture)
     {
         _scriptures.Remove(scripture);
